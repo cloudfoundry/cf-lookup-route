4da3329c2085b7b16ee835fdbdb28c791b16b91d
Daria <66593181+Dariquest@users.noreply.github.com>
2024-11-18 10:23:09 +0100

    Upload the files githubtools

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # cf-lookup-route
-Will soon be implemented: A plugin for the CF-CLI to quickly identify the CF application a certain route is pointing to.
+# Cloud Foundry Route Lookup Plugin
+
+This is a Cloud Foundry CLI plugin to quickly identify applications, a given route is pointing to.
+Note this will only show applications in organizations and spaces, that the logged-in user has permissions to view.
+The plugin also supports targeting to the organization and space of the applications, a given route is pointing to.
+
+## Installation
+
+1. Download the appropriate binary from [the Releases page](https://github.com/cloudfoundry/cf-lookup-route/releases).
+2. Run
+
+    ```sh
+    cf install-plugin -r CF-Community PATH_TO_ROUTE_LOOKUP_BIN
+    ```
+
+## Usage
+
+```
+$ cf lookup-route <my.example.com>
+Bound to:
+<org>/<space>/<app>
+
+# use -t to target the org/space containing the route
+$ cf lookup-route -t <my.example.com>
+Bound to:
+<org>/<space>/<app>
+Changed target to: <org>/<space>
+
+$ cf lookup-route <unknown.example.com>
+Error retrieving apps: Route <unknown.example.com> not found.
+```
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"code.cloudfoundry.org/cli/plugin"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/cloudfoundry/go-cfclient/v3/client"
+	"github.com/cloudfoundry/go-cfclient/v3/config"
+	"github.com/cloudfoundry/go-cfclient/v3/resource"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type lookupRoute struct{}
+
+func main() {
+	plugin.Start(new(lookupRoute))
+}
+
+func (l lookupRoute) Run(cliConnection plugin.CliConnection, args []string) {
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Printf("error: %s\n", err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	log.SetFlags(0)
+	flags := flag.NewFlagSet("lookup-route", flag.ContinueOnError)
+	target := flags.Bool("t", false, "Target the org/space containing this route")
+	err = flags.Parse(args[1:])
+	if err != nil {
+		return
+	}
+
+	if len(flags.Args()) == 0 || len(args) == 0 {
+		err = fmt.Errorf("please specify the required parameters")
+		return
+	}
+	hostName := flags.Args()[0]
+
+	hasApiEndpoint, err := cliConnection.HasAPIEndpoint()
+	if err != nil || !hasApiEndpoint {
+		err = fmt.Errorf("no API endpoint set")
+		return
+	}
+
+	cfc, err := createCfClient()
+	if err != nil {
+		return
+	}
+
+	route, err := findRoute(cfc, hostName)
+	if err != nil {
+		return
+	}
+
+	err = lookup(cfc, route, *target, cliConnection)
+	if err != nil {
+		return
+	}
+}
+
+func (l lookupRoute) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "lookup-route",
+		Version: plugin.VersionType{
+			Major: 0,
+			Minor: 1,
+			Build: 0,
+		},
+		Commands: []plugin.Command{
+			{
+				Name:     "lookup-route",
+				HelpText: "Cloud Foundry CLI plugin to identify the application, a given route is pointing to.",
+				UsageDetails: plugin.Usage{
+					Usage: "cf lookup-route [-t] ROUTE_URL",
+					Options: map[string]string{
+						"t": "Target the org/space containing the route",
+					},
+				},
+			},
+		},
+	}
+}
+
+func createCfClient() (*client.Client, error) {
+	cfg, err := config.NewFromCFHome()
+	if err != nil {
+		return &client.Client{}, err
+	}
+
+	cfc, err := client.New(cfg)
+	if err != nil {
+		return &client.Client{}, err
+	}
+
+	return cfc, nil
+}
+
+func findRoute(cfc *client.Client, query string) (*resource.Route, error) {
+	routeUrl, err := url.Parse(query)
+	if err != nil {
+		return &resource.Route{}, err
+	}
+	if routeUrl.Scheme == "" {
+		return &resource.Route{}, fmt.Errorf("please provide the url including the scheme")
+	}
+
+	parts := strings.SplitN(routeUrl.Hostname(), ".", 2)
+	if len(parts) < 2 {
+		return &resource.Route{}, fmt.Errorf("'%s' is not a domain", routeUrl.Hostname())
+	}
+	hostName := parts[0]
+	domainName := parts[1]
+
+	domainOpts := client.NewDomainListOptions()
+	domainOpts.Names.Values = append(domainOpts.Names.Values, domainName)
+	domains, err := cfc.Domains.ListAll(context.Background(), domainOpts)
+	if err != nil {
+		return &resource.Route{}, err
+	}
+
+	if len(domains) == 0 {
+		return &resource.Route{}, fmt.Errorf("error retrieving apps: route not found, domain '%s' is unknown", domainName)
+	}
+
+	opts := client.NewRouteListOptions()
+	opts.Hosts.Values = append(opts.Hosts.Values, hostName)
+	opts.DomainGUIDs.Values = append(opts.DomainGUIDs.Values, domains[0].GUID)
+
+	routes, err := cfc.Routes.ListAll(context.Background(), opts)
+	if err != nil {
+		return &resource.Route{}, err
+	}
+
+	if len(routes) == 0 {
+		opts.Hosts.Values = append(opts.Hosts.Values, "*")
+		routes, err = cfc.Routes.ListAll(context.Background(), opts)
+		if err != nil {
+			return &resource.Route{}, err
+		}
+		if len(routes) == 0 {
+			return &resource.Route{}, fmt.Errorf("error retrieving apps: Route '%s' not found", hostName)
+		}
+	}
+
+	return routes[0], nil
+}
+
+func lookup(cfc *client.Client, route *resource.Route, target bool, cliConnection plugin.CliConnection) error {
+	if route.Destinations == nil || len(route.Destinations) == 0 {
+		return fmt.Errorf("route not bound to any applications")
+	}
+
+	destinationCount := 0
+	for _, destination := range route.Destinations {
+		app, err := cfc.Applications.Get(context.Background(), *destination.App.GUID)
+		if err != nil {
+			return fmt.Errorf("route not bound to any applications")
+		}
+
+		space, org, err := cfc.Spaces.GetIncludeOrganization(context.Background(), app.Relationships.Space.Data.GUID)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Bound to:\nOrganization: %s (%s)\n", org.Name, org.GUID)
+		fmt.Printf("Space       : %s (%s)\n", space.Name, space.GUID)
+		fmt.Printf("App         : %s (%s)\n", app.Name, app.GUID)
+
+		if target && destinationCount == 0 {
+			fmt.Printf("Targeting an app's organization and space...\n")
+			_, err := cliConnection.CliCommand("target", "-o", org.Name, "-s", space.Name)
+			if err != nil {
+				fmt.Printf("targeting an app's organization and space failed\n")
+			}
+			fmt.Printf("Targeting an app's organization and space successful.\n")
+		}
+		destinationCount++
+	}
+
+	return nil
+}
