588b9fcc4ed64c8b0fa581e8bd0c43f79f3cce98
Daria <66593181+Dariquest@users.noreply.github.com>
2024-11-21 17:49:32 +0100

    Some more renamings

--- main.go
+++ main.go
@@ -188,12 +188,12 @@ func findRoute(cfc *client.Client, query string) (*resource.Route, error) {
 	return routes[0], nil
 }
 
-func getBatchEndIdx(numOfRouteDest int, packLength int, currentIdx int) int {
-	packEndIdx := currentIdx*packLength + packLength
-	if packEndIdx > numOfRouteDest {
-		packEndIdx = numOfRouteDest
+func getBatchEndIdx(numOfRouteDest int, batchCount int, currentIdx int) int {
+	batchEndIdx := currentIdx*batchCount + batchCount
+	if batchEndIdx > numOfRouteDest {
+		batchEndIdx = numOfRouteDest
 	}
-	return packEndIdx
+	return batchEndIdx
 }
 
 func resolveApps(cfc *client.Client, route *resource.Route) ([]*resource.App, error) {
